5060f45f843dc70f24129ed07dfd884b0021255a
Liisi M <liisi.muur@gmail.com>
2023-05-22 11:47:47 +0300

    Meritilt võetud InternView.vue

--- src/views/InternView.vue
+++ src/views/InternView.vue
@@ -1,12 +1,33 @@
 <template>
-see on Internide leht
-
-
-    <div class="row">
-        <div class="col mt-3">
-            <router-link to="/internships">Vaata uusi pakkumisi</router-link>
+    <div class="container">
+        <div class="row">
+            <div class="col col-7 mt-3 text-start">
+                <h1>Tere, KASUTAJANIMI</h1>
+            </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col mt-3 text-start">
+                <h3>Andmed:
+                    <font-awesome-icon :icon="['fas', 'user-pen']" size="xs" class="ms-5"/>
+                </h3>
+                <p>Nimi:</p>
+                <p>Email:</p>
+            </div>
+        </div>
+
+        <router-link to="/internships">Vaata pakkumisi</router-link>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <button  @click="addCv" type="button" class="btn btn-outline-success">Lisa CV</button>
+                <button  @click="addCoverLetter" type="button" class="btn btn-outline-success">Lisa motivatsioonikiri</button>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>

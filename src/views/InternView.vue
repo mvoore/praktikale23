6f9cb0d0628c76fdf97bf9ccddb22a19fe88cf6c
Administrator <merit.kadastik@gmail.com>
2023-05-31 11:33:37 +0300

    Cv ja Coverletter lähevad backi, internAndmed algus

--- src/views/InternView.vue
+++ src/views/InternView.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col col-7 mt-3 text-start">
-                <h2 id="username">Tere, {{ username }}</h2>
+                <h2 id="username">Tere, {{ internContact.username }}</h2>
             </div>
         </div>
     </div>
@@ -18,19 +18,29 @@
 
     <div class="container mt-5">
         <div class="row">
-            <div class="col mt-3 text-start">
+            <div class="col col-4 mt-3 text-start">
                 <h4>Andmed:
                     <font-awesome-icon :icon="['fas', 'user-pen']" size="xs" class="ms-5"/>
                 </h4>
-                <p>Nimi:</p>
-                <p>Email:</p>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Nimi</span>
+                    <input v-model="internContact.fullName" type="text" class="form-control"  >
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Email</span>
+                    <input v-model="internContact.email" type="text" class="form-control" >
+                </div>
+                <div>
+                    <button @click="" type="submit" class="btn btn-outline-primary btn-sm">Salvesta nimi ja email</button>
+                </div>
             </div>
             <div class=" mt-3 text-start">
                 <div>
                     <label for="cv">Lisa CV:</label>
                     <br>
-                    <CvInput @event-emit-base64=""/>
-                    <button type="submit" class="btn btn-outline-primary btn-sm text-end">Salvesta CV</button>
+                    <CvInput @event-emit-base64="setFileData"/>
+                    <button @click="addCv" type="submit" class="btn btn-outline-primary btn-sm text-end">Salvesta CV
+                    </button>
 
                 </div>
                 <br>
@@ -38,8 +48,9 @@
                 <div>
                     <label for="coverletter">Lisa motivatsioonikiri:</label>
                     <br>
-                    <CoverletterInput @event-emit-base64=""/>
-                    <button type="submit" class="btn btn-outline-primary btn-sm">Salvesta motivatsioonikiri</button>
+                    <CoverletterInput @event-emit-base64="setCoverletterFileData"/>
+
+                    <button @click="addCoverletter" type="submit" class="btn btn-outline-primary btn-sm">Salvesta motivatsioonikiri</button>
                 </div>
             </div>
         </div>
@@ -61,16 +72,82 @@
 </template>
 
 <script>
-import CvInput from "@/components/CvInput.vue";
-import CoverletterInput from "@/components/CoverletterInput.vue";
+import CvInput from "@/components/CvCoverletter/CvInput.vue";
+import CoverletterInput from "@/components/CvCoverletter/CoverletterInput.vue";
+import router from "@/router";
 
 export default {
     name: "internView",
     components: {
         CvInput,
         CoverletterInput
-    }
-}
+    },
 
+    data() {
+        return {
+            userId: sessionStorage.getItem('userId'),
+            internContact: {
+                username: '',
+                fullName: '',
+                email: ''
+            },
+            newCv: {
+                title: '',
+                fileData: ''
+            },
+            newCoverletter: {
+                title:'',
+                fileData:''
+            }
+        }
+    },
+    methods: {
+        addCv: function () {
+            this.$http.post("/intern/cv", this.newCv, {
+                    params: {
+                        userId: this.userId
+                    }
+                }
+            ).then(response => {
+                this.newCv = response.data
+
+            }).catch(error => {
+                const errorResponseBody = error.response.data
+            })
+        },
+        setFileData(fileData) {
+            this.newCv.fileData = fileData
+
+        },
+        addCoverletter: function () {
+            this.$http.post("/intern/coverletter", this.newCoverletter, {
+                    params: {
+                        userId: this.userId
+                    }
+                }
+            ).then(response => {
+               this.newCoverletter =response.data
+
+            }).catch(error => {
+                const errorResponseBody = error.response.data
+            })
+        },
+        setCoverletterFileData(fileData){
+            this.newCoverletter.fileData= fileData
+        },
+        getInternContact: function () {
+            this.$http.get("/intern", {
+                    params: {
+                        userId: this.userId
+                    }
+                }
+            ).then(response => {
+                this.internContact = response.data
+            }).catch(error => {
+                const errorResponseBody = error.response.data
+            })
+        },
+    },
+}
 
 </script>
